31c3a2b4f70691155d65768a0df7fbcc0fea09bf
0xc0debabe <kongminoo@naver.com>
2025-08-11 19:17:07 +0900

    first commit

---
+++ src/test/java/com/example/mogak_payment/MogakPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mogak_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogakPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
